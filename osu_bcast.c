78edae7ddd1bc879e526621a71671bf1a63a8611
flubknuy <spencergwwilliams@gmail.com>
2018-07-24 14:01:42 -0500

    fixed bcast file

--- osu_bcast.c
+++ osu_bcast.c
@@ -1,3 +1,4 @@
+#define BENCHMARK "OSU MPI%s Broadcast Latency Test"
 /*
  * Copyright (C) 2002-2018 the Network-Based Computing Laboratory
  * (NBCL), The Ohio State University.
@@ -7,359 +8,138 @@
  * For detailed copyright and licensing information, please refer to the
  * copyright file COPYRIGHT in the top level OMB directory.
  */
-#ifndef OSU_COLL_H
-#define OSU_COLL_H 1
-#endif
-
-#ifndef OSU_PT2PT_H
-#define OSU_PT2PT_H 1
-#endif
-
-#include <assert.h>
-#include <mpi.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <osu_util.h>
 #include <math.h>
-#include <string.h>
-#include <stdint.h>
-#include <getopt.h>
-#include <pthread.h>
-#include <inttypes.h>
-#include <sys/time.h>
-#include <limits.h>
 
+int main(int argc, char *argv[])
+{
+    int i = 0, rank, size;
+    int numprocs;
+    double avg_time = 0.0, max_time = 0.0, min_time = 0.0;
+    double latency = 0.0, t_start = 0.0, t_stop = 0.0;
+    double timer=0.0;
+    char *buffer=NULL;
+    int po_ret;
+    options.bench = COLLECTIVE;
+    options.subtype = LAT;
 
+    set_header(HEADER);
+    set_benchmark_name("osu_bcast");
+    po_ret = process_options(argc, argv);
 
-#ifdef _ENABLE_CUDA_
-#include "cuda.h"
-#include "cuda_runtime.h"
-#endif
+    if (PO_OKAY == po_ret && NONE != options.accel) {
+        if (init_accel()) {
+            fprintf(stderr, "Error initializing device\n");
+            exit(EXIT_FAILURE);
+        }
+    }
 
-#define MIN(a,b) ((a)<(b)?(a):(b))
-#define MAX(a,b) ((a)>(b)?(a):(b))
+    MPI_CHECK(MPI_Init(&argc, &argv));
+    MPI_CHECK(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
+    MPI_CHECK(MPI_Comm_size(MPI_COMM_WORLD, &numprocs));
 
-#ifdef _ENABLE_OPENACC_
-#   define OPENACC_ENABLED 1
-#   include <openacc.h>
-#else
-#   define OPENACC_ENABLED 0
-#endif
+    switch (po_ret) {
+        case PO_BAD_USAGE:
+            print_bad_usage_message(rank);
+            MPI_CHECK(MPI_Finalize());
+            exit(EXIT_FAILURE);
+        case PO_HELP_MESSAGE:
+            print_help_message(rank);
+            MPI_CHECK(MPI_Finalize());
+            exit(EXIT_SUCCESS);
+        case PO_VERSION_MESSAGE:
+            print_version_message(rank);
+            MPI_CHECK(MPI_Finalize());
+            exit(EXIT_SUCCESS);
+        case PO_OKAY:
+            break;
+    }
 
-#ifdef _ENABLE_CUDA_
-#   define CUDA_ENABLED 1
-#else
-#   define CUDA_ENABLED 0
-#endif
+    if(numprocs < 2) {
+        if (rank == 0) {
+            fprintf(stderr, "This test requires at least two processes\n");
+        }
 
-#ifdef _ENABLE_CUDA_KERNEL_
-#   define CUDA_KERNEL_ENABLED 1
-#else
-#   define CUDA_KERNEL_ENABLED 0
-#endif
+        MPI_CHECK(MPI_Finalize());
+        exit(EXIT_FAILURE);
+    }
 
-#ifndef BENCHMARK
-#   define BENCHMARK "MPI%s BENCHMARK NAME UNSET"
-#endif
+    if (options.max_message_size > options.max_mem_limit) {
+        if (rank == 0) {
+            fprintf(stderr, "Warning! Increase the Max Memory Limit to be able to run up to %ld bytes.\n"
+                            "Continuing with max message size of %ld bytes\n", 
+                            options.max_message_size, options.max_mem_limit);
+        }
+        options.max_message_size = options.max_mem_limit;
+    }
 
-#ifdef PACKAGE_VERSION
-#   define HEADER "# " BENCHMARK " v" PACKAGE_VERSION "\n"
-#else
-#   define HEADER "# " BENCHMARK "\n"
-#endif
+    if (allocate_memory_coll((void**)&buffer, options.max_message_size, options.accel)) {
+        fprintf(stderr, "Could Not Allocate Memory [rank %d]\n", rank);
+        MPI_CHECK(MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE));
+    }
+    set_buffer(buffer, options.accel, 1, options.max_message_size);
 
-#ifndef FIELD_WIDTH
-#   define FIELD_WIDTH 20
-#endif
+    print_preamble(rank);
 
-#ifndef FLOAT_PRECISION
-#   define FLOAT_PRECISION 2
-#endif
+    for(size=options.min_message_size; size <= options.max_message_size; size *= 2) {
+        if(size > LARGE_MESSAGE_SIZE) {
+            options.skip = options.skip_large; 
+            options.iterations = options.iterations_large;
+        }
 
-#define SYNC_MODE (UPC_IN_ALLSYNC | UPC_OUT_ALLSYNC)
+        int a, b, trials = 1000;
+        double avgk = 0, sum = 0, sum2 = 0;
+        double nums_ls[1000];
+        for(a = 0; a < trials; a++){
 
-#define CHECK(stmt)                                              \
-do {                                                             \
-   int errno = (stmt);                                           \
-   if (0 != errno) {                                             \
-       fprintf(stderr, "[%s:%d] function call failed with %d \n",\
-        __FILE__, __LINE__, errno);                              \
-       exit(EXIT_FAILURE);                                       \
-   }                                                             \
-   assert(0 == errno);                                           \
-} while (0)
+            timer=0.0;
+            for(i=0; i < options.iterations + options.skip ; i++) {
+                t_start = MPI_Wtime();
+                MPI_CHECK(MPI_Bcast(buffer, size, MPI_CHAR, 0, MPI_COMM_WORLD));
+                t_stop = MPI_Wtime();
 
-#define MPI_CHECK(stmt)                                          \
-do {                                                             \
-   int mpi_errno = (stmt);                                       \
-   if (MPI_SUCCESS != mpi_errno) {                               \
-       fprintf(stderr, "[%s:%d] MPI call failed with %d \n",     \
-        __FILE__, __LINE__,mpi_errno);                           \
-       exit(EXIT_FAILURE);                                       \
-   }                                                             \
-   assert(MPI_SUCCESS == mpi_errno);                             \
-} while (0)
+                if(i>=options.skip){
+                    timer+=t_stop-t_start;
+                }
+                MPI_CHECK(MPI_Barrier(MPI_COMM_WORLD));
 
-#define TIME() getMicrosecondTimeStamp()
-double getMicrosecondTimeStamp();
+            }
 
-void print_header_coll (int rank, int full) __attribute__((unused));
-void print_header_nbc (int rank, int full);
-void print_data (int rank, int full, int size, double avg_time, double
-min_time, double max_time, int iterations) __attribute__((unused));
-void print_data_nbc (int rank, int full, int size, double ovrl, double
-cpu, double comm, double wait, double init, int iterations);
+            MPI_CHECK(MPI_Barrier(MPI_COMM_WORLD));
 
-void allocate_host_arrays();
+            latency = (timer * 1e6) / options.iterations;
 
-void
-calculate_and_print_stats(int rank, int size, int numprocs,
-                          double timer, double latency,
-                          double test_time, double cpu_time,
-                          double wait_time, double init_time);
+            MPI_CHECK(MPI_Reduce(&latency, &min_time, 1, MPI_DOUBLE, MPI_MIN, 0,
+                    MPI_COMM_WORLD));
+            MPI_CHECK(MPI_Reduce(&latency, &max_time, 1, MPI_DOUBLE, MPI_MAX, 0,
+                    MPI_COMM_WORLD));
+            MPI_CHECK(MPI_Reduce(&latency, &avg_time, 1, MPI_DOUBLE, MPI_SUM, 0,
+                    MPI_COMM_WORLD));
+            avg_time = avg_time/numprocs;
+            sum += avg_time;
+            nums_ls[a] = avg_time;
+        }
+        avgk = sum/trials;
+        for(b = 0; b < trials; b++){
+            sum2 += pow(nums_ls[b] - avgk, 2);
+        }
+        sum2 = sum2/1000;
+        double sigma = sqrt(sum2);
+        print_statsk(rank, size, avg_time, avgk, sigma);
+    }
 
+    free_buffer(buffer, options.accel);
 
-enum mpi_req{
-    MAX_REQ_NUM = 1000
-};
+    MPI_CHECK(MPI_Finalize());
 
-#define BW_LOOP_SMALL 100
-#define BW_SKIP_SMALL 10
-#define BW_LOOP_LARGE 20
-#define BW_SKIP_LARGE 2
-#define LAT_LOOP_SMALL 10000
-#define LAT_SKIP_SMALL 100
-#define LAT_LOOP_LARGE 1000
-#define LAT_SKIP_LARGE 10
-#define COLL_LOOP_SMALL 1000
-#define COLL_SKIP_SMALL 100
-#define COLL_LOOP_LARGE 100
-#define COLL_SKIP_LARGE 10
-#define OSHM_LOOP_SMALL 1000
-#define OSHM_LOOP_LARGE 100
-#define OSHM_SKIP_SMALL 200
-#define OSHM_SKIP_LARGE 10
-#define OSHM_LOOP_SMALL_MR 500
-#define OSHM_LOOP_LARGE_MR 50
-#define OSHM_LOOP_ATOMIC 500
-#define UPC_LOOP_SMALL 1000
-#define UPC_LOOP_LARGE 100
-#define UPC_SKIP_SMALL 200
-#define UPC_SKIP_LARGE 10
+    if (NONE != options.accel) {
+        if (cleanup_accel()) {
+            fprintf(stderr, "Error cleaning up device\n");
+            exit(EXIT_FAILURE);
+        }
+    }
 
-#define MAX_MESSAGE_SIZE (1 << 22)
-#define MAX_MSG_SIZE_PT2PT (1<<20)
-#define MAX_MSG_SIZE_COLL (1<<20)
-#define MIN_MESSAGE_SIZE 1
-#define LARGE_MESSAGE_SIZE 8192
+    return EXIT_SUCCESS;
+}
 
-#define MAX_ALIGNMENT 65536
-#define MAX_MEM_LIMIT (512*1024*1024)
-#define MAX_MEM_LOWER_LIMIT (1*1024*1024)
-#define WINDOW_SIZE_LARGE 64
-#define MYBUFSIZE MAX_MESSAGE_SIZE
-#define ONESBUFSIZE ((MAX_MESSAGE_SIZE * WINDOW_SIZE_LARGE) + MAX_ALIGNMENT)
-#define MESSAGE_ALIGNMENT 64
-#define MESSAGE_ALIGNMENT_MR (1<<12)
-#define MYBUFSIZE_MR (MAX_MESSAGE_SIZE * OSHM_LOOP_LARGE_MR + MESSAGE_ALIGNMENT)
-
-enum po_ret_type {
-    PO_CUDA_NOT_AVAIL,
-    PO_OPENACC_NOT_AVAIL,
-    PO_BAD_USAGE,
-    PO_HELP_MESSAGE,
-    PO_VERSION_MESSAGE,
-    PO_OKAY,
-};
-
-enum accel_type {
-    NONE,
-    CUDA,
-    OPENACC,
-    MANAGED
-};
-
-enum target_type {
-    CPU,
-    GPU,
-    BOTH
-};
-
-enum benchmark_type {
-    COLLECTIVE,
-    PT2PT,
-    ONE_SIDED,
-    MBW_MR,
-    OSHM,
-    UPC,
-    UPCXX
-};
-
-enum test_subtype {
-    BW,
-    LAT,
-    LAT_MT,
-};
-
-enum test_synctype {
-    ALL_SYNC,
-    ACTIVE_SYNC
-};
-
-enum WINDOW {
-    WIN_CREATE=0,
-#if MPI_VERSION >= 3
-    WIN_ALLOCATE,
-    WIN_DYNAMIC
-#endif
-};
-
-/* Synchronization */
-enum SYNC {
-    LOCK=0,
-    PSCW,
-    FENCE,
-#if MPI_VERSION >= 3
-    FLUSH,
-    FLUSH_LOCAL,
-    LOCK_ALL,
-#endif
-};
-
-/*variables*/
-extern char const *win_info[20];
-extern char const *sync_info[20];
-
-extern MPI_Aint disp_remote;
-extern MPI_Aint disp_local;
-
-struct options_t {
-    enum accel_type accel;
-    enum target_type target;
-    int show_size;
-    int show_full;
-    size_t min_message_size;
-    size_t max_message_size;
-    size_t iterations;
-    size_t iterations_large;
-    size_t max_mem_limit;
-    size_t skip;
-    size_t skip_large;
-    size_t window_size_large;
-    int num_probes;
-    int device_array_size;
-
-    enum benchmark_type bench;
-    enum test_subtype  subtype;
-    enum test_synctype synctype;
-
-    char src;
-    char dst;
-    int num_threads;
-    char managedSend;
-    char managedRecv;
-    enum WINDOW win;
-    enum SYNC sync;
-
-    int window_size;
-    int window_varied;
-    int print_rate;
-    int pairs;
-};
-
-
-extern struct options_t options;
-
-
-/*
- * Non-blocking Collectives
- */
-double call_test(int * num_tests, MPI_Request** request);
-void allocate_device_arrays(int n);
-double dummy_compute(double target_secs, MPI_Request *request);
-void init_arrays(double seconds);
-double do_compute_and_probe(double seconds, MPI_Request *request);
-void free_host_arrays();
-
-#ifdef _ENABLE_CUDA_KERNEL_
-void free_device_arrays();
-#endif
-
-/*
- * Option Processing
- */
-int process_options (int argc, char *argv[]);
-
-/*
- * Print Information
- */
-void print_bad_usage_message (int rank);
-void print_help_message (int rank);
-void print_version_message (int rank);
-void print_preamble (int rank);
-void print_preamblek (int rank);
-void print_preamble_nbc (int rank);
-void print_stats (int rank, int size, double avg, double min, double max);
-void print_statsk (int rank, int size, double avg_time, double avgk, double sigma);
-void print_stats_nbc (int rank, int size, double ovrl, double cpu, double comm,
-                      double wait, double init, double test);
-/*
- * Memory Management
- */
-int allocate_memory_coll (void ** buffer, size_t size, enum accel_type type);
-void free_buffer (void * buffer, enum accel_type type);
-void set_buffer (void * buffer, enum accel_type type, int data, size_t size);
-
-/*
- * CUDA Context Management
- */
-int init_accel (void);
-int cleanup_accel (void);
-
-/*
- * Set Benchmark Properties
- */
-void set_header (const char * header);
-void set_benchmark_name (const char * name);
-void enable_accel_support (void);
-
-extern MPI_Request request[MAX_REQ_NUM];
-extern MPI_Status  reqstat[MAX_REQ_NUM];
-extern MPI_Request send_request[MAX_REQ_NUM];
-extern MPI_Request recv_request[MAX_REQ_NUM];
-
-#ifdef _ENABLE_CUDA_
-extern CUcontext cuContext;
-#endif
-
-
-#define DEF_NUM_THREADS 2
-#define MIN_NUM_THREADS 1
-#define MAX_NUM_THREADS 128
-
-#define WINDOW_SIZES {1, 2, 4, 8, 16, 32, 64, 128}
-#define WINDOW_SIZES_COUNT   (8)
-
-void usage_mbw_mr();
-int allocate_memory_pt2pt (char **sbuf, char **rbuf, int rank);
-void print_header_pt2pt (int rank, int type);
-void free_memory (void *sbuf, void *rbuf, int rank);
-void print_header(int rank, int full);
-void allocate_memory_one_sided(int, char *, char *, char **, char **,
-                     char **win_base, int, enum WINDOW, MPI_Win *);
-void free_memory_one_sided (void *, void *, MPI_Win, int);
-void allocate_atomic_memory(int, char *, char *, char *,
-                            char *, char **, char **, char **, char **,
-                            char **win_base, int, enum WINDOW, MPI_Win *);
-void free_atomic_memory (void *, void *, void *, void *, MPI_Win, int);
-void usage_one_sided (char const *);
-void print_header_one_sided (int, enum WINDOW, enum SYNC);
-
-void print_help_message_get_acc_lat (int);
-
-void print_header_pgas (const char *header, int rank, int full);
-void print_data_pgas (int rank, int full, int size, double avg_time, double min_time, double max_time, int iterations);
-void print_usage_pgas(int rank, const char * prog, int has_size);
-void print_version_pgas(const char *header);
-void usage_oshm_pt2pt(int myid);
-void wtime(double *t);
+/* vi: set sw=4 sts=4 tw=80: */
